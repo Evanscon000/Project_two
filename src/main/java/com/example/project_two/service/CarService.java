85fa074a53c15f2cba4dd8bc6f284fdf26649a39
Adam Stiavetti <adam.r.stiavetti.mil@army.mil>
2025-04-29 13:55:15 -0500

    finished car service test and service implimentation

--- src/main/java/com/example/project_two/service/CarService.java
+++ src/main/java/com/example/project_two/service/CarService.java
@@ -4,14 +4,37 @@ import com.example.project_two.entity.Car;
 import com.example.project_two.repository.CarRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CarService {
 
-    private final CarRepository repo;
+    private final CarRepository carRepository;
 
-    public CarService(CarRepository repo) {this.repo = repo;}
+    public CarService(CarRepository repo) {this.carRepository = repo;}
 
     public Car createNewCarEntityInDB(Car car) {
-        return repo.save(car);
+        return carRepository.save(car);
+    }
+
+    public List<Car> getAllCars() {return carRepository.findAll();
+    }
+
+    public Optional<Car> getCarById(Long id) {return carRepository.findById(id);
+    }
+
+    public Car updateCarById(Long id,Car updateCar) {
+        Car car = carRepository.findById(id).orElseThrow();
+        car.setMake(updateCar.getMake());
+        car.setModel(updateCar.getModel());
+        car.setYear(updateCar.getYear());
+        car.setPrice(updateCar.getPrice());
+        car.setUsed(updateCar.isUsed());
+        return carRepository.save(car);
+    }
+
+    public void deleteCar(Long id) {
+        carRepository.deleteById(id);
     }
 }
